c3799a7655cdb3b845d8c83b35730b12a3981ca2
Abdullah-01 <abdullah.alhokbani@gmail.com>
2024-11-16 20:01:11 +0300

    last

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import requests
-
+from PIL import Image
 
 API_URL = "https://lab-7-2.onrender.com/predict"  
 
-st.title("Machine Learning Prediction")
-st.write("Enter the features to make a prediction.")
+st.title("Player's Current price Prediction")
+st.image('img.gif')
+st.write("Enter the features to make a prediction for players current price.")
 
 assists = st.number_input("assists", min_value=0.0, max_value=1.0)
 minutes_played = st.number_input("minutes played ", min_value=0)
@@ -25,9 +26,15 @@ if st.button("Predict"):
     
 
     response = requests.post(API_URL, json=payload)
-    
+    x=""
     if response.status_code == 200:
         prediction = response.json()["pred"]
-        st.write(f"The prediction is: {prediction}")
+        if prediction == 0:
+            x = "Cheap price"
+        elif  prediction == 1:
+            x = "Good price"  
+        else:
+            x="High price"    
+        st.write(f"The prediction is: {x}")
     else:
         st.error("Error making prediction!")
